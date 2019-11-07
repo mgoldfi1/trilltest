0567666eea9743f624732a0c059a0c95939081e9
matt <matt@matts-iMac.local>
2019-11-07 13:10:06 -0800

    project init

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+const MainNavigator = createStackNavigator({
+  Home: {screen: HomeScreen}
+});
+
+const App = createAppContainer(MainNavigator);
+
+export default App;
