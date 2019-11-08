563612526a2c9273b77bab5191aa9b66a56e928c
matt <matt@matts-iMac.local>
2019-11-08 02:29:54 -0800

    firebase auth working

--- App.js
+++ App.js
@@ -1,53 +1,21 @@
-
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-import HomeScreen from './screens/HomeScreen';
-import  { createAppContainer } from 'react-navigation';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
-
-export class Home extends React.Component {
-  render() {
-  return (
-    <View>
-    <Text>"hello"</Text>
-    </View>
-  )
+import React from 'react'
+import { StyleSheet, Platform, Image, Text, View } from 'react-native'
+import { createSwitchNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
+// import the different screens
+import Loading from './screens/Loading'
+import SignUp from './screens/SignUp'
+import Login from './screens/Login'
+import Main from './screens/Main'
+// create our app's navigation stack
+const App = createSwitchNavigator(
+  {
+    Loading,
+    SignUp,
+    Login,
+    Main
+  },
+  {
+    initialRouteName: 'Loading'
   }
-}
-
-
-export class Social extends React.Component {
-  render() {
-  return (
-    <View>
-    <Text>social</Text>
-    </View>
-  )
-  }
-}
-
-
-export class Profile extends React.Component {
-  render() {
-  return (
-    <View>
-    <Text>profile</Text>
-    </View>
-  )
-  }
-}
-
-const TabNavigator = createBottomTabNavigator({
-  Home: Home,
-  Social: Social,
-  Profile: Profile
-});
-
-export default createAppContainer(TabNavigator)
+)
+export default createAppContainer(App)
