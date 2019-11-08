35c23742aeb99f4ba76d6ce6f622c9a65470712b
matt <matt@matts-iMac.local>
2019-11-07 16:10:29 -0800

    added in firebase package and bottom navigation, app working

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import {
   SafeAreaView,
@@ -8,13 +9,45 @@ import {
   StatusBar,
 } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+import  { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-const MainNavigator = createStackNavigator({
-  Home: {screen: HomeScreen}
+export class Home extends React.Component {
+  render() {
+  return (
+    <View>
+    <Text>"hello"</Text>
+    </View>
+  )
+  }
+}
+
+
+export class Social extends React.Component {
+  render() {
+  return (
+    <View>
+    <Text>social</Text>
+    </View>
+  )
+  }
+}
+
+
+export class Profile extends React.Component {
+  render() {
+  return (
+    <View>
+    <Text>profile</Text>
+    </View>
+  )
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Home: Home,
+  Social: Social,
+  Profile: Profile
 });
 
-const App = createAppContainer(MainNavigator);
-
-export default App;
+export default createAppContainer(TabNavigator)
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
-export default class HomeScreen extends Component {
+
+export class HomeScreen extends Component {
     render() {
         return (
             <View>
